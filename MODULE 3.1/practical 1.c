62f6304dd76ab97d616e5390f145444069625b84
Jeel Solanki <jeelsolanki39@gmail.com>
2024-08-24 14:08:38 +0530

    Jeel Solanki
    Se-Introduction To Programming module3.1-3.2-3.3

---
+++ MODULE 3.1/practical 1.c
@@ -0,0 +1,15 @@
+/*1 Display This Information using printf */   
+//1. Your Name   
+//2. Your Birth date  
+//3. Your Age   
+//4. Your Address  
+#include<stdio.h>
+int main()
+{
+	printf("\nYour Name = Solanki jeel");
+	printf("\nYour Birth Date = 12/07/2006");
+	printf("\nYour Age is = 19");
+	printf("\nYour Address = junagadh");
+	return 0;
+}
+
